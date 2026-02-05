2123a158609edde0322e59dd3cbfda89701cd577
Kaveesh Yoshitha <yoshithakaveesh83@gmail.com>
2026-02-05 14:31:17 +0530

    Logout Updated

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,10 +1,10 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import {
   createUser,
   findUserByEmail,
   getAllUsers,
-} from "../models/userModel.js";
+} from '../models/userModel.js';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -12,13 +12,13 @@ const sendToken = (res, user) => {
   const token = jwt.sign(
     { id: user.id, email: user.email, role: user.role },
     JWT_SECRET,
-    { expiresIn: "3d" },
+    { expiresIn: '3d' },
   );
 
-  res.cookie("token", token, {
+  res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'none',
     maxAge: 3 * 24 * 60 * 60 * 1000,
   });
 
@@ -30,23 +30,23 @@ export const registerUser = async (req, res) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
-      return res.status(400).json({ message: "Please fill all fields" });
+      return res.status(400).json({ message: 'Please fill all fields' });
     }
 
     const existingUSer = await findUserByEmail(email);
     if (existingUSer) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({ message: 'User already exists' });
     }
 
     const hashed = await bcrypt.hash(password, 10);
     const newUser = await createUser(username, email, hashed);
 
     res.status(201).json({
-      message: "User registered successfully",
+      message: 'User registered successfully',
       userId: newUser.insertId,
     });
   } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
 
@@ -55,22 +55,22 @@ export const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({ message: "Please fill all fields" });
+      return res.status(400).json({ message: 'Please fill all fields' });
     }
     const user = await findUserByEmail(email);
 
     if (!user) {
-      return res.status(404).json({ message: "User not found " });
+      return res.status(404).json({ message: 'User not found ' });
     }
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: 'Invalid credentials' });
     }
 
     sendToken(res, user);
 
     res.status(200).json({
-      message: "Login successful",
+      message: 'Login successful',
       // token: token,
       user: {
         id: user.id,
@@ -79,13 +79,18 @@ export const loginUser = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
 
 export const logout = (req, res) => {
-  res.clearCookie("token");
-  res.json({ message: "Logged out" });
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'none',
+  });
+
+  res.status(200).json({ message: 'Logged out' });
 };
 
 export const currentUser = async (req, res) => {
@@ -93,13 +98,13 @@ export const currentUser = async (req, res) => {
     const user = req.user;
 
     if (!user || !user.email) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res.status(401).json({ message: 'Unauthorized' });
     }
 
     const userDetails = await findUserByEmail(user.email);
 
     if (!userDetails) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: 'User not found' });
     }
 
     res.status(200).json({
@@ -110,7 +115,7 @@ export const currentUser = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
 
@@ -120,6 +125,6 @@ export const getUsers = async (req, res) => {
     const users = await getAllUsers(store_id);
     res.status(200).json({ users });
   } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
+    res.status(500).json({ message: 'Server error', error: error.message });
   }
 };
